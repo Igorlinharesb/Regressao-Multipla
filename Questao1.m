3a613adad300492c80923a12d4b193b12acaaa90
Igor Linhares <igorlinhares@alu.ufc.br>
2018-10-15 13:54:54 -0300

    Gráfico atualizado

--- Questao1.m
+++ Questao1.m
@@ -8,15 +8,12 @@
 clc;
 clear;
 close all;
-
 %Carrega base de dados
 load aerogerador.dat; 
 V = aerogerador (:,1);
 P = aerogerador (:,2);
-
 grau = input ('Digite o grau do modelo de regressão desejado: ');
 [Beta] = CalculaBeta(grau, aerogerador); 
-
     switch (grau)
         case 2
             % Gera o Polinômio:
@@ -28,7 +25,6 @@ grau = input ('Digite o grau do modelo de regressão desejado: ');
                 yest(i) = Beta(1) + Beta(2)*(V(i)) + Beta(3)*(V(i).^2);
                 i=i+1;
                 end
-          
         case 3 % Regressão Polinomial de Grau 3.
             % Gerando o Polinômio
                 x = 0:0.2:15;
@@ -39,7 +35,6 @@ grau = input ('Digite o grau do modelo de regressão desejado: ');
                 yest(i) = Beta(1) + Beta(2)*(V(i)) + Beta(3)*(V(i).^2) + Beta(4)*(V(i).^3);
                 i=i+1;
                 end
-                
         case 4 % Regressão Polinomial de Grau 4.
             % Gerando o Polinômio:
                 x = 0:0.2:15;
@@ -50,7 +45,6 @@ grau = input ('Digite o grau do modelo de regressão desejado: ');
                 yest(i) = Beta(1) + Beta(2)*(V(i)) + Beta(3)*(V(i).^2) + Beta(4)*(V(i).^3) + Beta(5)*(V(i).^4);
                 i=i+1;
                 end
-                
         case 5 % Regressão Polinomial de Grau 3.                
             % Gerando o Polinômio:
                 x = 0:0.2:15;
@@ -64,17 +58,16 @@ grau = input ('Digite o grau do modelo de regressão desejado: ');
         otherwise
             disp ('Este script só permite regressões de graus 2, 3, 4 e 5. Digite uma opção válida.')     
     end
-    
+   
     ymed = sum(P)/length(P);
     SQe = (P-(yest')).^2;
     Syy = (P-ymed).^2;
-    %Avaliando a qualidade do modelo pela métrica R2:
+    %Avaliando a qualidade do modelo pela métrica R2
     R2= 1-((sum(SQe)/sum(Syy)));
-    
-    %Avaliando a quadlidade do modelo pela métrica R2 Ajustada:
+    %Avaliando a quadlidade do modelo pela métrica R2 Ajustada
     R2aj = 1-((sum(SQe)/(length(P) - length (Beta)))/(sum(Syy)/(length(P)-1)));
-    
-    plot (x, Poli, 'y');
+    %Plotando o gráfico de dispersão
+    plot (x, Poli, 'r');
     hold on;
     plot (V, P, '.');
     grid on;
